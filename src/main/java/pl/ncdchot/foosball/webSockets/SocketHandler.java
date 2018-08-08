f182cb1049806fae140a82593d5c89ef82181c8a
Marek Pokropiński <marek.pokropinski@outlook.com>
2018-08-08 08:44:39 +0200

    Remove clients after send failure or after disconnect

--- src/main/java/pl/ncdchot/foosball/webSockets/SocketHandler.java
+++ src/main/java/pl/ncdchot/foosball/webSockets/SocketHandler.java
@@ -1,14 +1,12 @@
 package pl.ncdchot.foosball.webSockets;
 
-import org.springframework.web.socket.WebSocketSession;
-
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
-import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class SocketHandler extends TextWebSocketHandler {
@@ -16,18 +14,30 @@ public class SocketHandler extends TextWebSocketHandler {
 	private List<WebSocketSession> sessionList = new ArrayList<>();
 
 	public void sendMessageToAllClients(WebSocketMessage<String> message) {
-		try {
-			for (WebSocketSession client : sessionList) {
+		
+		List<WebSocketSession> toRemove = new ArrayList<>();
+		
+		for (WebSocketSession client : sessionList) {
+			try {
 				client.sendMessage(message);
-			}
-		} catch (IOException e) {
-			//TODO: Log this
-			System.out.println("Failed to send message");
+			} catch (IOException e) {
+				//TODO: Log this
+				System.out.println("Failed to send message");
+				toRemove.add(client);
+			}			
 		}
+		for (WebSocketSession client : toRemove) {
+			sessionList.remove(client);
+		}		
 	}
+		
 
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) {
 		sessionList.add(session);
+	}
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) {
+		sessionList.remove(session);
 	}
 }
