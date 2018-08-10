5a654bb0c3043c93b83b27640169400f6401970f
Paweł Kuklicz <p.kuklicz@gmail.com>
2018-08-10 10:30:30 +0200

    add prod configuration

--- src/main/java/pl/ncdchot/foosball/FoosballApplication.java
+++ src/main/java/pl/ncdchot/foosball/FoosballApplication.java
@@ -1,12 +1,26 @@
 package pl.ncdchot.foosball;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
-public class FoosballApplication {
+public class FoosballApplication extends SpringBootServletInitializer{
+//		implements WebApplicationInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FoosballApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(FoosballApplication.class, args);
 	}
+
+
+//	private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
+//		return builder.sources(FoosballApplication.class).bannerMode(Banner.Mode.OFF);
+//	}
 }
