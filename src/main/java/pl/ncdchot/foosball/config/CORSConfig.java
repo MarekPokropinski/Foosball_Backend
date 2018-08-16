31331a427c7c9ebbe8140980f95abafa207b15e9
JaroslawPokropinski <jarek.pokropinski@gmail.com>
2018-08-16 14:49:05 +0200

    Deleted deprecated code

--- src/main/java/pl/ncdchot/foosball/config/CORSConfig.java
+++ src/main/java/pl/ncdchot/foosball/config/CORSConfig.java
@@ -1,25 +1,15 @@
 package pl.ncdchot.foosball.config;
 
-
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@SuppressWarnings("deprecation")
 @Configuration
-public class CORSConfig {
+public class CORSConfig implements WebMvcConfigurer {
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
 
-			}
-		};
 	}
 }
