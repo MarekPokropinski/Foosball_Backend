5768657eceda2393f0bd60bcfb00776e2a5f863e
Marek Pokropiński <marek.pokropinski@outlook.com>
2018-08-21 15:20:18 +0200

    Added error handling

--- src/main/java/pl/ncdchot/foosball/services/implementations/TournamentSystemServiceImpl.java
+++ src/main/java/pl/ncdchot/foosball/services/implementations/TournamentSystemServiceImpl.java
@@ -4,8 +4,10 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Optional;
 
+import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import pl.ncdchot.foosball.services.TournamentSystemService;
@@ -13,6 +15,7 @@ import pl.ncdchot.foosball.services.TournamentSystemService;
 @Service
 public class TournamentSystemServiceImpl implements TournamentSystemService {
 	private static final String USER_URL = "http://hotdev:8080/usrmgmt/user/";
+	private static final Logger LOG = Logger.getLogger(TournamentSystemServiceImpl.class);
 	private RestTemplate restTemplate;
 
 	@Autowired
@@ -22,7 +25,13 @@ public class TournamentSystemServiceImpl implements TournamentSystemService {
 
 	@Override
 	public Optional<Long> getUserIdByNick(String nick) {
-		List<?> l = restTemplate.getForObject(USER_URL + "all", List.class);
+		List<?> l;
+		try {
+			l = restTemplate.getForObject(USER_URL + "all", List.class);
+		} catch (RestClientException e) {
+			LOG.warn("Failed to get users from user management");
+			return Optional.empty();
+		}
 		for (Object o : l) {
 			LinkedHashMap<?, ?> user = (LinkedHashMap<?, ?>) o;
 			if (user.get("nick").equals(nick)) {
