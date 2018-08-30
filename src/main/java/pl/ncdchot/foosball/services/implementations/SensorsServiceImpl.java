80be88ac000cb1c2d93853e0b2b11e57bbd2a075
Marek Pokropiński <marek.pokropinski@outlook.com>
2018-08-30 14:00:35 +0200

    Turn on and off sensors at game start and finish

--- src/main/java/pl/ncdchot/foosball/services/implementations/SensorsServiceImpl.java
+++ src/main/java/pl/ncdchot/foosball/services/implementations/SensorsServiceImpl.java
@@ -3,17 +3,21 @@ package pl.ncdchot.foosball.services.implementations;
 import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import pl.ncdchot.foosball.modelDTO.StatusDTO;
 import pl.ncdchot.foosball.services.SensorsService;
 
+@Service
 public class SensorsServiceImpl implements SensorsService {
 
 	private static final Logger LOG = Logger.getLogger(SensorsServiceImpl.class);
-	@Value("${sensors.endpoint}")
-	private static String SENSORS_ENDPOINT;
+	@Value("${sensors.url}")
+	private String SENSORS_URL;
+
+	private static final String SEND_ENDPOINT = "/send";
 
 	@Autowired
 	RestTemplate restTemplate;
@@ -22,7 +26,8 @@ public class SensorsServiceImpl implements SensorsService {
 	public void turnOn(boolean isOn) {
 		String status = isOn ? "on" : "off";
 		try {
-			restTemplate.postForObject(SENSORS_ENDPOINT, new StatusDTO(status), StatusDTO.class);
+			System.out.println(SENSORS_URL);
+			restTemplate.postForObject(SENSORS_URL, new StatusDTO(status), StatusDTO.class, SEND_ENDPOINT);
 		} catch (RestClientException e) {
 			LOG.warn("no connection with sensors");
 		}
